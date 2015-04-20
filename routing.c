fb0dd50bcf469ee879a53290ac6614df553612e0
Jonas Stendahl <jonas.stendahl@outlook.com>
2015-04-20 14:51:02 +0200

    String trimming in route_command

--- routing.c
+++ routing.c
@@ -10,6 +10,9 @@ bool route_command(struct string_array parameters, char **current_direction){
     char *first_parameter;
     if (parameters.size > 0){
         first_parameter = parameters.array[0];
+        if (first_parameter[strlen(parameters.array[0]) - 1] == '\n') {
+            first_parameter[strlen(parameters.array[0]) - 1] = '\0';
+        }
         if (!strcmp(first_parameter, "exit\n") || !strcmp(parameters.array[0], "exit")) {
             return true;
         }
