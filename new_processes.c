b27d297acf408776c9b0442a5ed78a4b174d2afe
Johan Arnör <johan.arnor@gmail.com>
2015-05-05 16:23:48 +0200

    Diz iz broken...

--- new_processes.c
+++ new_processes.c
@@ -3,24 +3,34 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <sys/times.h>
 #include <errno.h>
+#include <time.h>
 #include <sys/wait.h>
 #include "new_processes.h"
 
 void foreground_process(char *file, char **args){
     pid_t pid;
     int status;
+    struct timespec start, end;
+    uint64_t diff;
 
     if ((pid = fork()) == 0) {
         /*  In child process */
         if (execvp(file, args) == -1) {
             printf("Failed to start: %s\n", strerror(errno));
-            exit(1); /* Exit failiure */
+            exit(1); /* Kill confused child! */
         }
-        exit(0); /* Kill child! */
     } else {
         /* In parent process */
+        clock_gettime(CLOCK_MONOTONIC, &start); /* mark start time */
         waitpid(pid, &status, 0);
+        clock_gettime(CLOCK_MONOTONIC, &end);   /* mark the end time */
+        diff = end.tv_sec - start.tv_sec;
+
+        printf("Execution time was %f seconds for PID %d\n", ((double) time.tms_utime)/tics_per_second, pid);            
+
     }
 }
 
