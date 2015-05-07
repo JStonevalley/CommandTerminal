526a898598f71fd294c5eb0fb020945a660b9d26
Jonas Stendahl <jonas.stendahl@outlook.com>
2015-05-07 11:11:54 +0200

    printenv | sort | less

--- check_env.c
+++ check_env.c
@@ -9,27 +9,54 @@
 #include "check_env.h"
 
 bool checkEnv(){
-    pid_t printenv_pid;
-    int pipa[2];
-    if (-1 == pipe(pipa)){
+    pid_t printenv_pid, sort_pid;
+    int status, printenv_sort[2], sort_pager[2];
+    if (-1 == pipe(printenv_sort)){
         return false;
     }
-    printf("pipa[READ] = %d pipa[WRITE] = %d\n", pipa[READ], pipa[WRITE]);
+    if (-1 == pipe(sort_pager)){
+        return false;
+    }
     if ((printenv_pid = fork()) == 0) {
-        dup2(pipa[WRITE], STDOUT_FILENO);
-        close(pipa[READ]);
+        dup2(printenv_sort[WRITE], STDOUT_FILENO);
+        close(printenv_sort[READ]);
+        close(sort_pager[READ]);
+        close(sort_pager[WRITE]);
         if (execlp("printenv", "printenv", NULL) == -1) {
             printf("Failed to start printenv: %s\n", strerror(errno));
             exit(1);
         }
     }
-    else{
-        dup2(pipa[READ], STDIN_FILENO);
-        close(pipa[WRITE]);
-        if (execlp("sort", "sort", NULL) == -1) {
-            printf("Failed to start sort: %s\n", strerror(errno));
-            exit(1);
+    else {
+        if ((sort_pid = fork()) == 0) {
+            dup2(printenv_sort[READ], STDIN_FILENO);
+            dup2(sort_pager[WRITE], STDOUT_FILENO);
+            close(printenv_sort[WRITE]);
+            close(sort_pager[READ]);
+            if (execlp("sort", "sort", NULL) == -1) {
+                printf("Failed to start sort: %s\n", strerror(errno));
+                exit(1);
+            }
+        }
+        else {
+            if (printenv_pid != 0 && sort_pid != 0) {
+                dup2(sort_pager[READ], STDIN_FILENO);
+                close(printenv_sort[READ]);
+                close(printenv_sort[WRITE]);
+                close(sort_pager[WRITE]);
+                if (execlp("less", "less", NULL) == -1) {
+                    printf("Failed to start pager: %s\n", strerror(errno));
+                    exit(1);
+                }
+            }
         }
     }
+    close(printenv_sort[READ]);
+    close(printenv_sort[WRITE]);
+    close(sort_pager[READ]);
+    close(sort_pager[WRITE]);
+    wait(&status);
+    wait(&status);
+    wait(&status);
     return true;
 }
