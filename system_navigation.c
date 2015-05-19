cc514fa77b8ac1278bd3cb3de40ba7247bd4227d
Jonas Stendahl <jonas.stenahl@outlook.com>
2015-05-19 14:04:55 +0200

    Comments added to system_navigation

--- system_navigation.c
+++ system_navigation.c
@@ -4,11 +4,14 @@
 #include <errno.h>
 #include "system_navigation.h"
 
+/*
+	Navigate to the indicated directory.
+	If there is an error it is printed.
+*/
 void cd(char *dir) {
 	if (dir == NULL) {
 		dir = "/"; /* set to root */
 	}
-
     if (chdir(dir) == -1) {
         printf("Directory change failed: %s\n", strerror(errno));
     }
